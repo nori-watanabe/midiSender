f5ecbd31f1f5a1b5129eb23246c56e82c6d00ae9
function <fadedfever@gmail.com>
2019-01-30 18:43:31 +0900

    Initial Commit

---
+++ midiSender/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  midiSender
+//
+//  Created by mbp on 2019/01/30.
+//  Copyright © 2019 mbp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
